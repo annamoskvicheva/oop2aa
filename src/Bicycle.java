7a912cda6ecaf6334d5d58c8c8caa1c247498d29
annamoskviceva <Annab.spb@mail.ru>
2023-08-13 20:54:19 +0300

    inmit2

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Bicycle {
+public class Bicycle {
+    public String modelName;
+    public int wheelsCount;
+    public void updateTyre() {
+            System.out.println("Меняем покрышку");
+    }
 }
+
